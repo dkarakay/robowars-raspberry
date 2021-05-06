98744e7b4a774bcc10d73bbcc3cbdc81b64cf417
Deniz Karakay <deniz@karakay.me>
2021-05-06 20:07:04 +0300

    Servo added

--- motors.py
+++ motors.py
@@ -4,9 +4,9 @@ from random import randint
 from setup import get_from_firebase
 
 servo_pin = 17
-in1 = 24
+in1 = 22
 in2 = 23
-en = 25
+en = 27
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
@@ -17,13 +17,16 @@ GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
 
 p = GPIO.PWM(en, 1000)
-p.start(25)
+p.start(50)
+
+ang = 0
 
 pwm = GPIO.PWM(servo_pin, 50)
 pwm.start(0)
 
 
 def set_angle(ang: float):
+    print("ROTATING");
     duty = ang / 18 + 3
 
     GPIO.output(servo_pin, True)
@@ -34,25 +37,25 @@ def set_angle(ang: float):
 
 
 def forward():
-    print("forward")
+    print("FORWARD")
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
 
 
 def backward():
-    print("backward")
+    print("BACKWARD")
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
 
 
 def stop():
-    print("stop")
+    print("STOP")
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
 
 
 while True:
-    sleep(0.05)
+    sleep(0.2)
     objects = get_from_firebase()
 
     angle = float(objects['angle'])
@@ -68,10 +71,15 @@ while True:
             forward()
         elif direction == "backward":
             backward()
+        else:
+            stop()
+        
+        if ang != angle:
+            set_angle(angle)
+            ang = angle
     else:
         stop()
 
-    set_angle(angle)
 
 pwm.stop()
 GPIO.cleanup()
